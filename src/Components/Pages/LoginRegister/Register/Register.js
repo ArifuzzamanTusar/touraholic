442b9f4c3ac4154a69d1d51935460d4b07fc3f24
Arifuzzaman Tusar <arifuzzamantusar50@gmail.com>
2022-04-18 03:11:11 +0600

    register '

--- src/Components/Pages/LoginRegister/Register/Register.js
+++ src/Components/Pages/LoginRegister/Register/Register.js
@@ -1,11 +1,112 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Col, Container, Row } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword, useUpdateProfile } from 'react-firebase-hooks/auth';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import auth from '../../../../firebase.init';
 
 import "../LoginSignup.css";
 import SocialLogin from '../SocialLogin/SocialLogin';
 
 const Register = () => {
+    const [createUserWithEmailAndPassword, user] = useCreateUserWithEmailAndPassword(auth, { sendEmailVerification: true });
+    const [updateProfile, updating, updateError] = useUpdateProfile(auth);
+
+
+    const navigate = useNavigate();
+    const location = useLocation();
+    const from = location.state?.from?.pathname || '/';
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [rePassword, setRePassword] = useState('');
+
+    const [nameError, setNameError] = useState('');
+    // const [emailError, setEmailError] = useState('');
+    const [passwordError, setPasswordError] = useState('');
+    const [passwordMatchError, setPasswordMatchError] = useState('');
+
+    // --- 
+    // Set registration error 
+    const [regError, setRegError] = useState();
+
+    // Validation 
+    const handleName = (event) => {
+        let getName = event.target.value;
+        if (getName.length > 3) {
+            setName(getName);
+            setNameError(false);
+        }
+        else {
+            setNameError(
+                <small className='errorText'>Name Too Short</small>
+            )
+        }
+
+    }
+    const handleEmail = (event) => {
+        let getEmail = event.target.value;
+        setEmail(getEmail);
+
+
+    }
+    const handlePassword = (event) => {
+        let getPassword = event.target.value;
+        if (getPassword.length > 5) {
+            setPassword(getPassword);
+            setPasswordError(false);
+        }
+        else {
+            setPasswordError(
+                <small className='errorText'>Must be 6 Character</small>
+            )
+        }
+
+
+    }
+    const handleRePassword = (event) => {
+        let getRePassword = event.target.value;
+        if (getRePassword === password) {
+            setRePassword(password);
+            setPasswordMatchError(false);
+
+        }
+        else {
+            setPasswordMatchError(
+                <small className='errorText'>Password Didn't match</small>
+
+            );
+
+        }
+
+    }
+    if (user) {
+        navigate(from, { replace: true })
+    }
+    if (updating) {
+        return ("loading...");
+    }
+
+    // ----------- 
+    const handleSignup = async (event) => {
+        event.preventDefault();
+        console.log(name);
+        if (password.length > 5 && password === rePassword) {
+            await createUserWithEmailAndPassword(email, password);
+            await updateProfile({ displayName: name });
+            console.log(user.displayName);
+        }
+        else {
+            setRegError(
+                <small className='errorText'>Please Check errors {updateError}</small>
+            )
+        }
+
+    }
+
+
+
+
     return (
         <div id='page-login'>
             <div className='login-reg py-5'>
@@ -18,18 +119,21 @@ const Register = () => {
                         <Col md={5}>
                             <div className="form-body">
 
-                                <form >
+                                <form onSubmit={handleSignup}>
                                     <label> Name</label>
-                                    <input type="text" required />
+                                    <input type="text" required onKeyUp={handleName} className={nameError ? 'hasError' : 'noError'} />
+                                    {nameError}
                                     <label> Email</label>
-                                    <input type="email" required />
+                                    <input type="email" required onKeyUp={handleEmail} />
                                     <label> Pasword</label>
-                                    <input type="password" required />
+                                    <input type="password" required onKeyUp={handlePassword} className={passwordError ? 'hasError' : 'noError'} />
+                                    {passwordError}
                                     <label> Confirm Pasword</label>
-                                    <input type="password" required />
+                                    <input type="password" required onKeyUp={handleRePassword} />
+                                    {passwordMatchError}
 
-
-                                    <Button className='Log_in col-12 my-3'>Create Account</Button>
+                                    <Button type="submit" className='Log_in col-12 my-3'>Create Account</Button>
+                                    {regError}
 
                                 </form>
 
